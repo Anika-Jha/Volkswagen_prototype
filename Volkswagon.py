b4a417c04f5531e13b84f09699df884973d0774f
Anika Jha <113995788+Anika-Jha@users.noreply.github.com>
2025-03-13 19:18:48 +0530

    Update Volkswagon.py

--- Volkswagon.py
+++ Volkswagon.py
@@ -9,7 +9,7 @@ import streamlit as st
 import time
 
 # Step 1: Prepare Driving Data
-
+# taking random values 
 def generate_driving_data():
     data = {
         'speed': [30, 60, 45, 70, 55, 80, 40, 65],
